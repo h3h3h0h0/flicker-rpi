4b8cac62c01b20ffe25ea1ac7cb78259b6edacf8
h4h4h1h1 <yx2wang@uwaterloo.ca>
2024-05-27 23:21:04 -0400

    e

--- caminterface.cc
+++ caminterface.cc
@@ -2,11 +2,24 @@
 
 using namespace std;
 
-CamInterface::CamInterface(): cam{new raspicam::RaspiCam()} {
-    if(cam->open()) { 
-        cerr<<"CAMERA NOT FOUND!"<<endl;
-    }
-    
-    
+const int GET_ITERATIONS = 8; //how many images worth of noise?
 
+CamInterface::CamInterface() {
+    if(cam.open()) { 
+        throw CamNotFoundException();
+    }
+    //sleep(3) //not sure if this works
+}
+
+void CamInterface::get(CryptoPP::Keccak_512 &hash) {
+    hash.Restart();
+    cam.grab();
+    size_t sz = cam.getImageTypeSize (raspicam::RASPICAM_FORMAT_RGB);
+    for(int i = 0; i < GET_ITERATIONS; i++) {
+        cam.grab();
+        unique_ptr<unsigned char> data(new unsigned char[sz]);
+        cam.retrieve(data.get(), raspicam::RASPICAM_FORMAT_RGB);
+        hash.Update(data.get(), sz);
+        memset(data.get(), 0, sz); //destroy the data after you are done
+    }
 }
--- caminterface.h
+++ caminterface.h
@@ -2,11 +2,22 @@
 #include <ctime>
 #include <iostream>
 #include <fstream>
+#include <memory>
+#include <cryptopp/keccak.h>
+#include <functional>
 
 using namespace std;
 
+
 class CamInterface {
-    raspicam::RaspiCam *cam;
+
+    raspicam::RaspiCam cam;
+    
+    public:
+    class CamNotFoundException : public exception {};
+
     CamInterface();
+    void get(CryptoPP::Keccak_512 &hash);
+
 
 };
