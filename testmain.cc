dc0199329b1a8c390e33d95b6eb2fa0c0de8124e
h4h4h1h1 <yx2wang@uwaterloo.ca>
2024-06-01 16:54:19 -0400

    small test

---
+++ testmain.cc
@@ -0,0 +1,33 @@
+#include "caminterface.h"
+#include "osinterface.h"
+
+#include "cryptopp/keccak.h"
+
+using namespace std;
+
+uint32_t extract(CryptoPP::SecByteBlock sbb, size_t ind) {
+    uint32_t res = 0;
+    res += static_cast<int>(sbb[ind]);
+    res = res << 8;
+    res += static_cast<int>(sbb[ind+1]);
+    res = res << 8;
+    res += static_cast<int>(sbb[ind+2]);
+    res = res << 8;
+    res += static_cast<int>(sbb[ind+3]);
+    return res;
+}
+
+int main() {
+    CryptoPP::Keccak_512 k;
+    CamInterface cam();
+    OSInterface osi();
+    CryptoPP::SecByteBlock out(8192);
+    for(int i = 0; i < 10000; i++) {
+        cam.get(k);
+        osi.get(k);
+        k.TruncatedFinal(out, 8192);
+        for(int j = 0; j < 8192; j += 4) {
+            cout<<extract(out, j)<<endl;
+        }
+    }
+}
